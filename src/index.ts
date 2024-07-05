5bd89a1a17183e2e14ef390882cee928e7ff53d4
Manraj Chauhan <m900413089@gmail.com>
2024-07-05 20:58:57 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-// src/index.ts
+
 import express from 'express';
 import path from 'path';
 
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'views' folder
+
 app.use(express.static(path.join(__dirname, 'views')));
 
 // Expose an endpoint to serve the generatedData.json file
